d16aa3cd837fa071f09a35ebb3682dcabe3e2d3e
= <jwparker284@gmail.com>
2023-07-29 08:09:16 -0700

    added color picker, and border for selecting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 // Write your Color component here
+import { useState } from "react";
+const Color = (props) => {
+
+  let tempClassName = props.color
+  props.color === props.currentColor ? tempClassName += " selected" : tempClassName = props.color;
+  
+  return <div className={tempClassName}
+  onClick={
+    () => {props.setSelectedColor(props.color)
+      
+    
+    }
+  }
+  
+  ></div>
+}
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("violet");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+      <Color color="violet" setSelectedColor={setSelectedColor} currentColor = {selectedColor} />
+      <Color color="black" setSelectedColor = {setSelectedColor} currentColor = {selectedColor}  />
+      <Color color="orange" setSelectedColor= {setSelectedColor} currentColor = {selectedColor}  />{
+      /* colors go here */
+      
+      }</div>
     </div>
   );
 };
